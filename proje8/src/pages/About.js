b0b8a2d609fc1a1e9c53dd052715b889f7c5f4d4
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2024-06-08 18:37:21 +0200

    envoi du projet8 sur github

---
+++ proje8/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <p>bbbrjbfj;hbjgkbjgbfhdkfjjihbnrbvjdbfihhbcsbf;nkfnfdjjfjf
+            </p>
+        </div>
+    );
+};
+
+export default About;
