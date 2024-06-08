8b25e0be6bb7197a8537c2820cdbad0f153e8a3e
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2024-06-08 19:51:23 +0200

    stylisation de la navigation

--- proje8/src/components/Navigation.js
+++ proje8/src/components/Navigation.js
@@ -6,10 +6,13 @@ const Navigation = () => {
     <div className="navigation">
       <img src="./logo.png" alt="Logo" />
       <ul>
-        <NavLink to="/">
+        <NavLink
+          to="/"
+          className={(nav) => (nav.isActive ? "nav-active" : " ")}
+        >
           <li>Accueil</li>
         </NavLink>
-        <NavLink to="/about">
+        <NavLink to="/about"className={(nav) => (nav.isActive ? "nav-active" : " ")}>
           <li> A propos</li>
         </NavLink>
       </ul>
