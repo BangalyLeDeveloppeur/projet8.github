c4d755964ad7862625901ba8b62bc6db97860245
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2024-06-10 00:55:21 +0200

    organisation de la structure du contenu de la description de la page apropos

--- proje8/src/components/Description.js
+++ proje8/src/components/Description.js
@@ -3,7 +3,7 @@ import React from "react";
 const Description = () => {
   return (
     <div>
-      <section className="annonce">
+      <div className="annonce">
         <div className="description">
           <h3>Fiabilité</h3>
           <img src="./arrow.png" alt="image fiabilité" />
@@ -13,6 +13,8 @@ const Description = () => {
           photos sont conformes aux logements, et toutes les informations sont
           régulièrement vérifiées par nos équipes.
         </p>
+      </div>
+      <div className="annonce">
         <div className="description">
           <h3>Respect</h3>
           <img src="./arrow.png" alt="image fiabilité" />
@@ -22,23 +24,32 @@ const Description = () => {
           comportement discrimination de perturbation du voisinage entrainera
           une exclusion de notre plateforme.
         </p>
+      </div>
+      <div className="annonce">
         <div className="description">
           <h3>Service</h3>
           <img src="./arrow.png" alt="image fiabilité" />
         </div>
         <p>
-        La bienveillance fait partie des valeurs fondactrices de kasa. Tout
+          La bienveillance fait partie des valeurs fondactrices de kasa. Tout
           comportement discrimination de perturbation du voisinage entrainera
           une exclusion de notre plateforme.
         </p>
+      </div>
+      <div className="annonce">
         <div className="description">
           <h3>Sécurité</h3>
           <img src="./arrow.png" alt="image fiabilité" />
         </div>
         <p>
-        La sécurité est la priorité de kasa. Aussi bien pour nos hotes que pour les voyageurs, chaque logement correspond aux de sécurité établis par nos services. En laissant une note aussi bien l'hote qu'au locataire, cela permet à nos équipes de vérifer que les standarts sont bien respectés. Nous organisons égalemnet des ateliers sur la sécurité domestique pour nos hotes.
+          La sécurité est la priorité de kasa. Aussi bien pour nos hotes que
+          pour les voyageurs, chaque logement correspond aux de sécurité établis
+          par nos services. En laissant une note aussi bien l'hote qu'au
+          locataire, cela permet à nos équipes de vérifer que les standarts sont
+          bien respectés. Nous organisons égalemnet des ateliers sur la sécurité
+          domestique pour nos hotes.
         </p>
-      </section>
+      </div>
     </div>
   );
 };
