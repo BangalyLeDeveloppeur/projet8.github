9822a438cc4770db161a52ea184d8a6787672885
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2024-06-09 02:11:46 +0200

    ajustement du style photoapropos

--- proje8/src/pages/About.js
+++ proje8/src/pages/About.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
-import Photapropos from '../components/Photapropos';
+import React from "react";
+import Navigation from "../components/Navigation";
+import Photapropos from "../components/Photapropos";
 
 const About = () => {
-    return (
-        <div>
-            <Navigation />
-            <Photapropos />
-            <h1>A propos</h1>
-            <p>bbbrjbfj;hbjgkbjgbfhdkfjjihbnrbvjdbfihhbcsbf;nkfnfdjjfjf
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <Navigation />
+      <Photapropos />
+
+      <h1>A propos</h1>
+      <p>bbbrjbfj;hbjgkbjgbfhdkfjjihbnrbvjdbfihhbcsbf;nkfnfdjjfjf</p>
+    </div>
+  );
 };
 
 export default About;
