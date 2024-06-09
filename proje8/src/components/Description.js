aa2ed9df9d2118890c6906f18e779e0f800b9a92
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2024-06-10 00:39:20 +0200

    styles sur la partie description sur la page apropos

---
+++ proje8/src/components/Description.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Description = () => {
+  return (
+    <div>
+      <section>
+        <div className="description">
+          <h3>Fiabilité</h3>
+          <img src="./arrow.png" alt="image fiabilité" />
+        </div>
+        <p>
+          Les annonces postées sur kasa garantissent une fiabilité totale. Les
+          photos sont conformes aux logements, et toutes les informations sont
+          régulièrement vérifiées par nos équipes.
+        </p>
+        <div className="description">
+          <h3>Respect</h3>
+          <img src="./arrow.png" alt="image fiabilité" />
+        </div>
+        <p>
+          La bienveillance fait partie des valeurs fondactrices de kasa. Tout
+          comportement discrimination de perturbation du voisinage entrainera
+          une exclusion de notre plateforme.
+        </p>
+        <div className="description">
+          <h3>Service</h3>
+          <img src="./arrow.png" alt="image fiabilité" />
+        </div>
+        <p>
+        La bienveillance fait partie des valeurs fondactrices de kasa. Tout
+          comportement discrimination de perturbation du voisinage entrainera
+          une exclusion de notre plateforme.
+        </p>
+        <div className="description">
+          <h3>Sécurité</h3>
+          <img src="./arrow.png" alt="image fiabilité" />
+        </div>
+        <p>
+        La sécurité est la priorité de kasa. Aussi bien pour nos hotes que pour les voyageurs, chaque logement correspond aux de sécurité établis par nos services. En laissant une note aussi bien l'hote qu'au locataire, cela permet à nos équipes de vérifer que les standarts sont bien respectés. Nous organisons égalemnet des ateliers sur la sécurité domestique pour nos hotes.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default Description;
--- proje8/src/pages/About.js
+++ proje8/src/pages/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import Photapropos from "../components/Photapropos";
+import Description from "../components/Description";
 
 const About = () => {
   return (
     <div>
       <Navigation />
       <Photapropos />
+      < Description/>
 
       <h1>A propos</h1>
       <p>bbbrjbfj;hbjgkbjgbfhdkfjjihbnrbvjdbfihhbcsbf;nkfnfdjjfjf</p>
