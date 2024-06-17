1d94cfa7f546b6fc379894c539e903df9cce9780
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2024-06-17 20:52:34 +0200

    creation la div pour les chevrons de la page appartement

--- proje8/src/components/Logementimage.js
+++ proje8/src/components/Logementimage.js
@@ -1,27 +1,30 @@
 import { useParams } from "react-router-dom";
 import livres from "../data/livres.json";
 
-
-
-
-const Logement = ({logement}) => {
+const Logement = ({ logement }) => {
   const pictures = logement.pictures;
-  
 
   return (
     <div>
       <div className="loge-img">
-      {pictures?.slice(0, 1).map((pictures, index) => {
-        return (
-          <img src={pictures} alt={pictures.title} key={(pictures, index)} />
+        {pictures?.slice(0, 1).map((pictures, index) => {
+          return (
+            <img src={pictures} alt={pictures.title} key={(pictures, index)} />
           );
-          })}
-      <img src="./arrow.png" alt="image arrow" />
-      <img src="./arrow.png" alt="image arrow" />
-         </div>
+        })}
+
+        <div className="logement-arrow">
+          <span>
+            <i class="fa-solid fa-chevron-up"></i>
+          </span>
+          <span>
+            <i class="fa-solid fa-chevron-up"></i>
+          </span>
+        </div>
+      </div>
 
       <p>Voici le logoment avec id {logement?.id} </p>
-      
+
       <p> {logement?.equipments} </p>
       <p> {logement?.location} </p>
     </div>
