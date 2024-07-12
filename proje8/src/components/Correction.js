7a893b810a284f15e642376f17477bf7ede40878
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2024-07-12 19:45:18 +0200

    creation du data pour paragraphe sur la page apropo

---
+++ proje8/src/components/Correction.js
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const Slide = ({ logement }) => {
+  const pictures = logement.pictures
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+  // j'utilise userState pour manipiler les elements dans mon tableau//
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0
+    const newIndex = isFirstSlide ? pictures.length - 1 : currentIndex - 1
+    setCurrentIndex(newIndex)
+  }
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === pictures.length - 1
+    const newIndex = isLastSlide ? 0 : currentIndex + 1
+    setCurrentIndex(newIndex)
+  }
+
+  return (
+    <div>
+      <div className="loge-img">
+        {pictures && pictures.length > 1 && (
+          <div className="logement-arrow">
+            <button onClick={prevSlide}>
+              <i className="fa-solid fa-chevron-up"></i>
+            </button>
+
+            <button onClick={nextSlide}>
+              <i className="fa-solid fa-chevron-right chevron-droite"></i>
+            </button>
+            <span>
+              {currentIndex + 1}/{pictures.length - 1}
+            </span>
+          </div>
+        )}
+        {pictures && pictures.length > 0 && (
+          <img
+            src={pictures[currentIndex]}
+            alt={`Slide ${currentIndex}`}
+            key={currentIndex}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Slide
