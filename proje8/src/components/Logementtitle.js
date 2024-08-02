ae874554ed993c513d15d28002aff7e37ad0dca4
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2024-08-02 13:29:15 +0200

    ajustement du style appartement

--- proje8/src/components/Logementtitle.js
+++ proje8/src/components/Logementtitle.js
@@ -3,7 +3,9 @@ import React from "react";
 const Logementtitle = ({logement}) => {
   return (
     <div className="logement">
+      <div className="titl-h">
       <h2 className="logoement-title">{logement.title}</h2>
+      </div>
       <p className="logement-ville">{logement.location}</p>
    
     </div>
